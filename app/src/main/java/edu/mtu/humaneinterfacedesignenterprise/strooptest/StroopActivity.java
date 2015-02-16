a5c77a0e6abdea7fd6ac972fb97d62037a0f3d33
Eric Kosovec <ejkosovec@gmail.com>
2015-02-15 23:07:30 -0500

    Changed timings and added menu
    The menu is a sliding menu for now that swaps between two images. The
    transparent cards are not completely transparent when displayed. I
    believe this to be the background itself causing the problem, but needs
    to be researched more.

--- app/src/main/java/edu/mtu/humaneinterfacedesignenterprise/strooptest/StroopActivity.java
+++ app/src/main/java/edu/mtu/humaneinterfacedesignenterprise/strooptest/StroopActivity.java
@@ -2,11 +2,16 @@ package edu.mtu.humaneinterfacedesignenterprise.strooptest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 
@@ -17,53 +22,27 @@ import com.google.android.glass.widget.CardScrollView;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Eric Kosovec
- */
 public class StroopActivity extends Activity {
+    public static final short FOURTY_KM = 0;
+    public static final short SIXTY_KM = 1;
+
     private static final String TAG = StroopActivity.class.getSimpleName();
 
     private CardScrollView mCardScroller;
-
-    private boolean autoSwipe;
-	
-    private int currentCard;
-	
     private List<CardBuilder> cards;
-
-    // Visible for testing.
-    public CardScrollView getScroller() {
-        return mCardScroller;
-    }
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
 
-        autoSwipe = true;
-
         // keep the display from turning off
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        createCards(this);
+        cards = new ArrayList<CardBuilder>();
 
-        if (autoSwipe) {
-            autoSwipeTest(7000, 1);
-        }
-
-        else {
-            manualSwipeTest();
-        }
-    }
-
-    /**
-     * Perform a stroop test, whereby the user
-     * must swipe to get from card to card.
-     */
-    private void manualSwipeTest() {
-        if (cards == null) {
-            createCards(this);
-        }
+        // Stroop test images
+        cards.add(createCard(this, R.drawable.kmhr40));
+        cards.add(createCard(this, R.drawable.kmhr60));
 
         mCardScroller = new CardScrollView(this);
         mCardScroller.setAdapter(new CardAdapter(cards));
@@ -74,120 +53,6 @@ public class StroopActivity extends Activity {
         setCardScrollerListener();
 
         setContentView(mCardScroller);
-    }
-
-    /**
-     * Perform a stroop test, whereby the user
-     * need not swipe through the cards. Instead
-     * the cards will change by themselves.
-     *
-     * @param ms     - Time in between cards in milliseconds.
-     * @param cycles - How many times to go through the cards.
-     */
-    private void autoSwipeTest(int ms, int cycles) {
-        if (!autoSwipe) {
-            return;
-        }
-
-        if (ms <= 0) {
-            ms = 2000;
-        }
-
-        if (cycles <= 0) {
-            cycles = 1;
-        }
-
-        if (cards == null) {
-            createCards(this);
-        }
-
-        currentCard = 0;
-
-        Handler mHandler = new Handler();
-
-        // Repeats the cards cycles amount of times.
-        int limit = cards.size() * cycles;
-
-        // Set up events to run to display the cards in succession after a
-        // period of time.
-        for (int i = 1; i <= limit; ++i) {
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    updateDisplay();
-                }
-            }, ms * i);
-            /* 1000 ms. = 1 sec.
-             * Runs given milliseconds after start. Milliseconds * index gives
-             * appropriate time for later cards.
-             */
-        }
-    }
-
-    /**
-     * Displays the next card and allows for cycling through cards,
-     * if desired.
-     */
-    private void updateDisplay() {
-        if (cards == null) {
-            return;
-        }
-
-        if (currentCard == cards.size()) {
-            currentCard = 0;
-        }
-
-        setContentView(cards.get(currentCard).getView());
-        ++currentCard;
-    }
-
-    /**
-     * Create list of API demo cards.
-     */
-    private void createCards(Context context) {
-        cards = new ArrayList<CardBuilder>();
-
-        // Stroop test images
-        cards.add(createCard(context, R.drawable.one));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.two));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.three));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.four));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.five));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.six));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.seven));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.eight));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.nine));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.ten));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.eleven));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.twelve));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.thirteen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.fourteen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.fifteen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.sixteen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.seventeen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.eighteen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.nineteen));
-        cards.add(createCard(context, R.drawable.black));
-        cards.add(createCard(context, R.drawable.twenty));
-        cards.add(createCard(context, R.drawable.black));
     }
 
     private static CardBuilder createCard(Context context, int drawable) {
@@ -205,7 +70,7 @@ public class StroopActivity extends Activity {
 
     @Override
     protected void onPause() {
-	    super.onPause();
+        super.onPause();
         if (mCardScroller != null) {
             mCardScroller.deactivate();
         }
@@ -214,9 +79,15 @@ public class StroopActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-	    if (mCardScroller != null) {
+        if (mCardScroller != null) {
             mCardScroller.deactivate();
         }
+    }
+
+    private void newActivity(boolean test1) {
+        Intent intent = new Intent(this, StroopTests.class);
+        intent.putExtra(StroopActivity.class.getName(), (test1 ? FOURTY_KM : SIXTY_KM));
+        startActivity(intent);
     }
 
     // Different types of activities can be shown, when tapped on a card.
@@ -230,12 +101,16 @@ public class StroopActivity extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.d(TAG, "Clicked view at position " + position + ", row-id " + id);
                 int soundEffect = Sounds.SELECTED;
-
-                // Tap to move to next card
-                mCardScroller.setSelection(++position);
                 AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
                 am.playSoundEffect(soundEffect);
 
+                if (position == 1) {
+                    newActivity(false);
+                }
+
+                else {
+                    newActivity(true);
+                }
             }
         });
     }
---
+++ app/src/main/java/edu/mtu/humaneinterfacedesignenterprise/strooptest/StroopTests.java
@@ -0,0 +1,231 @@
+package edu.mtu.humaneinterfacedesignenterprise.strooptest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.google.android.glass.widget.CardBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StroopTests extends Activity {
+    private int currentCard;
+
+    private List<CardBuilder> cards;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        short test;
+
+        Intent intent = getIntent();
+
+        if (intent == null) {
+            test = 0;
+        }
+
+        else {
+            // Gets data sent from the menu. No value given, defaults to 0.
+            test = intent.getShortExtra(StroopActivity.class.getName(), (short)0);
+        }
+
+        currentCard = 0;
+
+        // keep the display from turning off
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        createCards(this);
+
+        if (test == 0) {
+            fourtyKMPerHour();
+        }
+
+        else {
+            sixtyKMPerHour();
+        }
+
+        setContentView(createCard(this, R.drawable.transparent).getView());
+    }
+
+    private void fourtyKMPerHour() {
+        int[] times = new int[12];
+
+        times[0] = 35;
+
+        /* Generate times from start stroop cards should appear. */
+        for (int inc = 1; inc < times.length; ++inc) {
+            if (inc == 6) {
+                times[inc] = times[inc - 1] + 18;
+            }
+            else {
+                times[inc] = times[inc - 1] + 14;
+            }
+        }
+
+        Handler mHandler = new Handler();
+
+        // Set up events to run to display the cards in succession after a
+        // period of time.
+        for (int i = 0; i < times.length; ++i) {
+            // Stroop card.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    updateDisplay();
+                }
+            }, times[i] * 1000);
+
+            // Transparent card
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    updateDisplay();
+                }
+            }, (times[i] + 3) * 1000);
+        }
+    }
+
+    private void sixtyKMPerHour() {
+        double[] times = new double[12];
+
+        times[0] = 23.5;
+
+        /* Generate times from start stroop cards should appear. */
+        for (int inc = 1; inc < times.length; ++inc) {
+            if (inc == 6) {
+                times[inc] = times[inc - 1] + 10.5;
+            }
+            else {
+                times[inc] = times[inc - 1] + 9;
+            }
+        }
+
+        Handler mHandler = new Handler();
+
+        // Set up events to run to display the cards in succession after a
+        // period of time.
+        for (int i = 0; i < times.length; ++i) {
+            // Stroop card.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    updateDisplay();
+                }
+            }, (int)(times[i] * 1000.));
+
+            // Transparent card
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    updateDisplay();
+                }
+            }, (int)((times[i] + 3) * 1000.));
+        }
+    }
+
+    /**
+     * Create list of API demo cards.
+     */
+    private void createCards(Context context) {
+        cards = new ArrayList<CardBuilder>();
+
+        // Stroop test images
+        cards.add(createCard(context, R.drawable.one));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.two));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.three));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.four));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.five));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.six));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.seven));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.eight));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.nine));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.ten));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.eleven));
+        cards.add(createCard(context, R.drawable.transparent));
+        cards.add(createCard(context, R.drawable.twelve));
+        cards.add(createCard(context, R.drawable.transparent));
+    }
+
+    /**
+     * Perform a stroop test, whereby the user
+     * need not swipe through the cards. Instead
+     * the cards will change by themselves.
+     *
+     * @param ms     - Time in between cards in milliseconds.
+     * @param cycles - How many times to go through the cards.
+     */
+    private void autoSwipeTest(int ms, int cycles) {
+
+        if (ms <= 0) {
+            ms = 2000;
+        }
+
+        if (cycles <= 0) {
+            cycles = 1;
+        }
+
+        if (cards == null) {
+            createCards(this);
+        }
+
+        currentCard = 0;
+
+        Handler mHandler = new Handler();
+
+        // Repeats the cards cycles amount of times.
+        int limit = cards.size() * cycles;
+
+        // Set up events to run to display the cards in succession after a
+        // period of time.
+        for (int i = 1; i <= limit; ++i) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    updateDisplay();
+                }
+            }, ms * i);
+            /* 1000 ms. = 1 sec.
+             * Runs given milliseconds after start. Milliseconds * index gives
+             * appropriate time for later cards.
+             */
+        }
+    }
+
+    /**
+     * Displays the next card and allows for cycling through cards,
+     * if desired.
+     */
+    private void updateDisplay() {
+        if (cards == null) {
+            return;
+        }
+
+        if (currentCard == cards.size()) {
+            currentCard = 0;
+        }
+
+        setContentView(cards.get(currentCard).getView());
+        ++currentCard;
+    }
+
+    private static CardBuilder createCard(Context context, int drawable) {
+        return new CardBuilder(context, CardBuilder.Layout.TITLE)
+                .addImage(drawable);
+    }
+}
