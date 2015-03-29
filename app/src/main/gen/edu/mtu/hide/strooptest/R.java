17ce4dbd43cd3de36a7c48dae8cdd62e1976a0fd
kjtimmer <kjtimmer@mtu.edu>
2015-03-29 17:00:14 -0400

    Changing package location

--- app/src/main/gen/edu/mtu/hide/strooptest/R.java
+++ app/src/main/gen/edu/mtu/hide/strooptest/R.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package edu.mtu.humaneinterfacedesignenterprise.strooptest;
+package edu.mtu.hide.strooptest;
 
 /* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
 public final class R {
